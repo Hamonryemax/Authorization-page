77f71464e36d3141a21584c870159f1247aab91f
Maxim Yakushin <hamonryemax@gmail.com>
2022-06-15 16:36:39 +0400

    работа в прогрессе

--- VP.js
+++ VP.js
@@ -1,12 +1,44 @@
-document.getElementById('button').addEventListener('click', showLoading);
+const button = document.getElementById('button');
+button.addEventListener('click', authorization);
+
+function authorization() {
+    const login = document.getElementById('login').value;
+    const pass = document.getElementById('pass').value;
+    if (login === '' || pass === '') {
+        alert('Не заполнены поля ввода!');
+        return;
+    }
+    getVerification(login, pass);
+}
+
+function getVerification(login, pass) {
+    const arrayLogin = login.split('');
+    const arrayPass = pass.split('');
+    if (arrayLogin[1] === arrayPass[2] && arrayLogin[2] === arrayPass[1]
+        && arrayLogin[arrayLogin.length - 2] === arrayPass[arrayPass.length - 3]
+        && arrayLogin[arrayLogin.length - 3] === arrayPass[arrayPass.length - 2]) {
+        showLoading();
+        console.log('true');
+    } else {
+        showLoading();
+        console.log('false');
+    }
+}
 
 function showLoading() {
     const loading = document.querySelector('.loading');
-    const remove = () => {
+    const remove = function () {
         loading.classList.remove('loading-visible');
-    }
+    };
     loading.classList.add('loading-visible');
-    setTimeout(remove, 3000);
+    setTimeout(remove, getRandom(1000, 3000));
 }
 
 
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function con() {
+    console.log('Выведись вовремя, пж');
+}
