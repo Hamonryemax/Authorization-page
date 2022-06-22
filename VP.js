a3c94eff54d7e870706dd5976530b0f86516d301
Maxim Yakushin <hamonryemax@gmail.com>
2022-06-22 13:30:01 +0400

    pred-final version project

--- VP.js
+++ VP.js
@@ -1,39 +1,55 @@
 const button = document.getElementById('button');
-button.addEventListener('click', authorization);
+button.addEventListener('click', showLoadingAndAuthorization);
 
-function authorization() {
+function showLoadingAndAuthorization() {
     const login = document.getElementById('login').value;
     const pass = document.getElementById('pass').value;
     if (login === '' || pass === '') {
-        alert('Не заполнены поля ввода!');
         return;
     }
-    getVerification(login, pass);
-}
-
-function getVerification(login, pass) {
-    const arrayLogin = login.split('');
-    const arrayPass = pass.split('');
-    if (arrayLogin[1] === arrayPass[2] && arrayLogin[2] === arrayPass[1]
-        && arrayLogin[arrayLogin.length - 2] === arrayPass[arrayPass.length - 3]
-        && arrayLogin[arrayLogin.length - 3] === arrayPass[arrayPass.length - 2]) {
-        showLoading();
-        console.log('true');
-    } else {
-        showLoading();
-        console.log('false');
-    }
-}
-
-function showLoading() {
     const loading = document.querySelector('.loading');
     const remove = function () {
         loading.classList.remove('loading-visible');
+        authorization();
     };
     loading.classList.add('loading-visible');
     setTimeout(remove, getRandom(1000, 3000));
 }
 
+function authorization() {
+    const login = document.getElementById('login').value;
+    const pass = document.getElementById('pass').value;
+    getVerification(login, pass);
+}
+
+function getVerification(login, pass) {
+    const button = document.getElementById('button');
+    const arrayLogin = login.split('');
+    const arrayPass = pass.split('');
+    if (arrayLogin[1] === arrayPass[2] && arrayLogin[2] === arrayPass[1]
+        && arrayLogin[arrayLogin.length - 2] === arrayPass[arrayPass.length - 3]
+        && arrayLogin[arrayLogin.length - 3] === arrayPass[arrayPass.length - 2]) {
+        const succ = document.querySelector('.success');
+        const removeSuccess = function () {
+            succ.classList.remove('show-success');
+            button.classList.remove('button-none');
+        };
+        succ.classList.add('show-success');
+        button.classList.add('button-none');
+        setTimeout(removeSuccess, 3000);
+        console.log('true');
+    } else {
+        const error = document.querySelector('.error');
+        const removeError = function () {
+            error.classList.remove('show-error');
+            button.classList.remove('button-none');
+        }
+        error.classList.add('show-error');
+        button.classList.add('button-none');
+        setTimeout(removeError, 3000);
+        console.log('false');
+    }
+}
 
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
